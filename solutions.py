f2f3c2bbe4a2d45684f6a63c1be1c357056017b6
Nicky García <nick.garc.96@gmail.com>
2017-06-26 10:01:26 -0500

    Created first instance of the solution world model in solutions.py

---
+++ solutions.py
@@ -0,0 +1,155 @@
+#-*- coding: utf-8 -*-
+import system, util
+
+class Algorithms:
+    def dfs(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+
+        fringe = util.Stack()
+        visited = []
+        plan = [problem.getStartState()]
+
+        fringe.push( (problem.getStartState(), plan) )
+        while(not fringe.isEmpty()):
+            current, planToNode = fringe.pop()
+            if problem.isGoal(current):
+                #print("Visited: " + str(visited)) #DEBUG
+                return planToNode
+            if current not in visited:
+                visited.append(current)
+                for child in problem.getSuccessors(current):
+                    fringe.push( (child[0], planToNode + [child[0]] ) )
+
+    def dfs2(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+
+        pila = util.Stack()
+        visitados = []
+        plan = [problem.getStartState()]
+
+        pila.push( (problem.getStartState(), plan) )
+
+        while( not pila.isEmpty() ):
+            edo_act, plan = pila.pop()
+
+            if not problem.isGoal(edo_act):
+                if edo_act not in visitados:
+                    visitados.append(edo_act)
+
+                    for hijo in problem.getSuccessors(edo_act):
+                        pila.push( (hijo[0], plan + [hijo[0]]) )
+            else:
+                break
+        return plan
+
+    def bfs(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+        cola = util.Queue()
+        visitados = []
+        plan = [problem.getStartState()]
+
+        cola.push( (problem.getStartState(), plan) )
+
+        while( not cola.isEmpty() ):
+            edo_act, plan = cola.pop()
+
+            if not problem.isGoal(edo_act):
+                if edo_act not in visitados:
+                    visitados.append(edo_act)
+
+                    for hijo in problem.getSuccessors(edo_act):
+                        cola.push( (hijo[0], plan + [hijo[0]]) )
+            else:
+                break
+        #print visitados #DEBUG
+        return plan
+
+    def ucs(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+        pathCost = 0
+        curr_node = (problem.getStartState(), [problem.getStartState()], pathCost)
+        fringe = util.PriorityQueue()
+        fringe.update(curr_node, 0)
+        visited = []
+        cost = 0
+
+        while not fringe.isEmpty():
+            curr_node = fringe.pop()
+            if problem.isGoal(curr_node[0]):
+                #print("cost: " + str(curr_node[2])) #DEBUG
+                return curr_node[1]
+            if curr_node[0] not in visited:
+                visited.append(curr_node[0])
+                for child in problem.getSuccessors(curr_node[0]):
+                    child = (child[0], curr_node[1] + [child[0]], child[1] + curr_node[2])
+                    #print(child) ->
+                    #('B', ['A', 'B'], 3)
+                    #('C', ['A', 'C'], 1)
+                    if child[0] not in visited:
+                        fringe.update(child, child[2])
+                    #print fringe.heap #DEBUG
+
+        #util.raiseNotDefined()
+
+    def astar(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+        pathCost = 0 + problem.heuristic((problem.getStartState(), [problem.getStartState()], 0))
+        curr_node = (problem.getStartState(), [problem.getStartState()], pathCost)
+        fringe = util.PriorityQueue()
+        fringe.update(curr_node, 0)
+        visited = []
+        cost = 0
+        while not fringe.isEmpty():
+            curr_node = fringe.pop()
+            if problem.isGoal(curr_node[0]):
+                #print("cost: " + str(curr_node[2])) #DEBUG
+                return curr_node[1]
+                if curr_node[0] not in visited:
+                    visited.append(curr_node[0])
+                    for child in problem.getSuccessors(curr_node[0]):
+                        child = (child[0], curr_node[1] + [child[0]], child[1] + curr_node[2])
+                        #print(child) ->
+                        #('B', ['A', 'B'], 3)
+                        #('C', ['A', 'C'], 1)
+                        if child[0] not in visited:
+                            fringe.update(child, child[2] + problem.heuristic(child[0]))
+                            #print fringe.heap #DEBUG
+                            #util.raiseNotDefined()
+
+class SpaceSolution:
+    #Base goal world model
+    world = {
+        ('El Rosario', 0):[ ('Tacuba', 0), ("Instituto del Petróleo", 0) ],
+        ("Instituto del Petróleo", 0):[ ('La Raza', 0), ("Deportivo 18 de Marzo", 0), ('El Rosario', 0) ],
+        ("Deportivo 18 de Marzo", 0):[ ('La Raza', 0), ("Instituto del Petróleo", 0), ('Martin Carrera', 0) ],
+        ('Martin Carrera', 0):[ ('Deportivo 18 de Marzo', 0), ('Consulado', 0) ],
+        ("La Raza", 0): [("Deportivo 18 de Marzo", 0), ("Instituto del Petróleo", 0), ('Consulado', 0), ("Guerrero", 0)],
+        ("Consulado", 0): [("Martin Carrera", 0), ("La Raza", 0), ("Morelos", 0)],
+        ("Guerrero", 0): [('Garibaldi', 0), ('La Raza', 0), ("Hidalgo", 0)],
+        ("Garibaldi", 0) : [('Guerrero', 0), ('Morelos', 0), ('Bellas Artes', 0)],
+        ("Morelos", 0) : [("Garibaldi", 0), ("Consulado", 0), ("Candelaria", 0), ("San Lázaro", 0)],
+        ("Tacuba", 0) : [('Hidalgo', 0), ("Tacubaya", 0), ("El Rosario", 0)],
+        ("Hidalgo", 0) : [("Tacuba", 0), ("Balderas", 0), ("Guerrero", 0), ("Bellas Artes", 0)],
+        ("Bellas Artes", 0) : [("Hidalgo", 0), ("Garibaldi", 0), ("Pino Suárez", 0)],
+        ("Tacubaya", 0) : [("Tacuba", 0), ("Mixcoac", 0)],
+        ("Balderas", 0) : [("Hidalgo", 0), ("Centro Médico", 0), ("Pino Suárez", 0), ("Tacubaya", 0)],
+        ("Pino Suárez", 0) : [("Bellas Artes", 0), ("Chabacano", 0), ("Candelaria", 0)],
+        ("Candelaria", 0) : [("Pino Suárez", 0), ("Jamaica", 0), ("San Lázaro", 0), ("Morelos", 0)],
+        ("San Lázaro", 0) : [("Candelaria", 0), ("Morelos", 0)],
+        ("Mixcoac", 0) : [("Tacubaya", 0), ("Zapata", 0)],
+        ("Centro Médico", 0) : [("Chabacano", 0), ("Balderas", 0), ("Zapata", 0)],
+        ("Chabacano", 0) : [("Jamaica", 0), ("Pino Suárez", 0), ("Ermita", 0), ("Centro Médico", 0)],
+        ("Jamaica", 0) : [("Chabacano", 0), ("Candelaria", 0), ("Santa Anita", 0)],
+        ("Zapata", 0) : [("Mixcoac", 0), ("Centro Médico", 0), ("Ermita", 0)],
+        ("Ermita", 0) : [("Zapata", 0), ("Chabacano", 0)]
+    }
+
+    problem = system.Problem( start = 'Malaga',
+                              goal = 'Santiago',
+                              custom_graph = world,
+                            )
---
+++ system.py
@@ -0,0 +1,400 @@
+#-*- coding: utf-8 -*-
+"""
+This graph solving system was created by Nicky García Fierros
+for PROTECO's Inteligencia Artificial 2017-2 course.
+
+Licensing Information:  You are free to use or extend these projects for
+educational purposes provided that (1) you do not distribute or publish
+solutions, (2) you retain this notice, and (3) you provide clear
+attribution to Nicky García Fierros, including a link to
+https://github.com/kotoromo.
+
+"""
+
+import util, solutions
+
+class Problem:
+
+    graph_2 = {
+        ('A', 1):[('B', 2)],
+        ('B', 2):[('C', 1), ('D', 5)],
+        ('D', 5):[('E', 1), ('F', 3)],
+        ('E', 1):[('D', 5)],
+        ('F', 3):[('D', 5)]
+    }
+
+    """
+    Grafo con costos
+    """
+    graph_1 = {
+        ('A', 1):[('B', 1)],
+        ('B', 1):[('C', 1), ('D', 1), ('A', 1)],
+        ('C', 1):[('B', 1)],
+        ('D', 1):[('E', 1), ('F', 1), ('B', 1)],
+        ('E', 1):[('D', 1)],
+        ('F', 1):[('D', 1)]
+    }
+
+    graph_3 = {
+        ('A', 0):[('B', 3), ('C', 1)],
+        ('B', 0):[('C', 1), ('D', 2), ('A', 3)],
+        ('C', 0):[('B', 1), ('A', 1), ('F', 7)],
+        ('D', 0):[('E', 2), ('F', 1), ('B', 2)],
+        ('E', 0):[('D', 2)],
+        ('F',0):[('D', 1), ('C', 7)]
+    }
+
+    """
+    Problem abstraction.
+    Arguments:
+        'space' = state space
+        'method' = solving method/function
+        'goal' = goal node
+        'start' = start node
+        'goal_fn' = custom goal function, if not specified
+        uses one that checks for equality between the state given
+        and the goal state defined within the problem.
+    """
+    def __init__(self, *args, **kwargs):
+        # Dictionary which states the available default graphs.
+        spaces = {'g1': self.graph_1, 'g2': self.graph_2, 'g3': self.graph_3}
+
+        if kwargs != None:
+            if kwargs.get('use_graph') is not None:
+                self.state_space = kwargs.get('space')
+            else:
+                #self.custom_graph = kwargs.get('use_graph')
+                self.state_space = kwargs.get('use_graph')
+
+            self.goal_state = kwargs.get('goal')
+            self.start_state = kwargs.get('start')
+            self.goal_function = kwargs.get('goal_fn')
+            self.heuristic = kwargs.get('heur')
+
+        if self.goal_function is None:
+            self.goal_function = self.defaultIsGoal
+
+        if self.state_space is None:
+            self.state_space = 'g1'
+            print("Using graph: " + self.state_space)
+        elif (self.state_space is None):
+            self.state_space = self.custom_graph
+        else:
+            print("Using graph: " + self.state_space)
+
+        if spaces is not None:
+            self.state_space = spaces[self.state_space]
+        else:
+            self.state_space = self.custom_graph
+
+        self.nodes_expanded = 0
+
+    """
+    Successor function.
+    Given a node, returns the list of successors associated with it.
+    """
+    def getSuccessors(self, state):
+        self.nodes_expanded += 1
+        #print("Node expanded:" + state) #DEBUG
+        print (self.state_space)
+        return self.state_space[self.findKey(state)]
+
+    """
+    Sets the heuristic function
+    """
+    def setHeuristic(self, h):
+        self.heuristic = h
+
+    """
+    Default goal check function.
+    Given a node, returns whether the node given is the same as the goal state.
+    """
+    def defaultIsGoal(self, state):
+        return self.goal_state == state
+
+    """
+    Custom goal check function.
+    Returns true if the given state is a goal.
+    """
+    def isGoal(self, state):
+        return self.goal_function(state)
+
+    """
+    Start state getter.
+    Returns the start state as defined by the problem.
+    """
+    def getStartState(self):
+        return self.start_state
+
+    """
+    System is probably badly designed. This is the soulution I came up with.
+    Method which finds the appropiate key for the given state.
+    """
+    def findKey(self, node_letter):
+        keys = self.state_space.keys()
+        for tuple in keys:
+            if node_letter == tuple[0]:
+                return tuple
+
+    def restartCounter(self):
+        self.nodes_expanded = 0
+
+    def getNodesExpanded(self):
+        a = self.nodes_expanded
+        self.restartCounter()
+        return a
+
+    def getSolutionCost(self, solution):
+        cost = 0
+        if solution is None:
+            return 0
+
+        for i in range(0, len(solution)-1):
+            #('A', 0):[('B', 3), ('C', 1)]
+            # state_space = {(NODE, COST): [LIST OF TUPLES]}
+            current = solution[i]
+            successors = self.getSuccessors(current)
+
+            #finding tuple with value
+            for tuple in successors:
+                if solution[i+1] == tuple[0]:
+                    cost+=tuple[1]
+
+        return cost
+
+
+
+class Solver:
+    def dfs(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+
+        fringe = util.Stack()
+        visited = []
+        plan = [problem.getStartState()]
+
+        fringe.push( (problem.getStartState(), plan) )
+        while(not fringe.isEmpty()):
+            current, planToNode = fringe.pop()
+            if problem.isGoal(current):
+                #print("Visited: " + str(visited)) #DEBUG
+                return planToNode
+            if current not in visited:
+                visited.append(current)
+                for child in problem.getSuccessors(current):
+                    fringe.push( (child[0], planToNode + [child[0]] ) )
+
+    def dfs2(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+
+        pila = util.Stack()
+        visitados = []
+        plan = [problem.getStartState()]
+
+        pila.push( (problem.getStartState(), plan) )
+
+        while( not pila.isEmpty() ):
+            edo_act, plan = pila.pop()
+
+            if not problem.isGoal(edo_act):
+                if edo_act not in visitados:
+                    visitados.append(edo_act)
+
+                    for hijo in problem.getSuccessors(edo_act):
+                        pila.push( (hijo[0], plan + [hijo[0]]) )
+            else:
+                break
+        return plan
+
+    def bfs(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+        cola = util.Queue()
+        visitados = []
+        plan = [problem.getStartState()]
+
+        cola.push( (problem.getStartState(), plan) )
+
+        while( not cola.isEmpty() ):
+            edo_act, plan = cola.pop()
+
+            if not problem.isGoal(edo_act):
+                if edo_act not in visitados:
+                    visitados.append(edo_act)
+
+                    for hijo in problem.getSuccessors(edo_act):
+                        cola.push( (hijo[0], plan + [hijo[0]]) )
+            else:
+                break
+        #print visitados #DEBUG
+        return plan
+
+        util.raiseNotDefined()
+
+    def ucs(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+        pathCost = 0
+        curr_node = (problem.getStartState(), [problem.getStartState()], pathCost)
+        fringe = util.PriorityQueue()
+        fringe.update(curr_node, 0)
+        visited = []
+        cost = 0
+
+        while not fringe.isEmpty():
+            curr_node = fringe.pop()
+            if problem.isGoal(curr_node[0]):
+                #print("cost: " + str(curr_node[2])) #DEBUG
+                return curr_node[1]
+            if curr_node[0] not in visited:
+                visited.append(curr_node[0])
+                for child in problem.getSuccessors(curr_node[0]):
+                    child = (child[0], curr_node[1] + [child[0]], child[1] + curr_node[2])
+                    #print(child) ->
+                    #('B', ['A', 'B'], 3)
+                    #('C', ['A', 'C'], 1)
+                    if child[0] not in visited:
+                        fringe.update(child, child[2])
+                    #print fringe.heap #DEBUG
+
+        #util.raiseNotDefined()
+
+    def astar(self, problem):
+        problem.restartCounter()
+        #Escribe tu código aquí
+
+        print problem.getStartState()
+        pathCost = 0 + problem.heuristic((problem.getStartState(), [problem.getStartState()], 0))
+        curr_node = (problem.getStartState(), [problem.getStartState()], pathCost)
+        fringe = util.PriorityQueue()
+        fringe.update(curr_node, 0)
+        visited = []
+        cost = 0
+
+        while not fringe.isEmpty():
+            curr_node = fringe.pop()
+            if problem.isGoal(curr_node[0]):
+                #print("cost: " + str(curr_node[2])) #DEBUG
+                return curr_node[1]
+            if curr_node[0] not in visited:
+                visited.append(curr_node[0])
+                for child in problem.getSuccessors(curr_node[0]):
+                    child = (child[0], curr_node[1] + [child[0]], child[1] + curr_node[2])
+                    #print(child) ->
+                    #('B', ['A', 'B'], 3)
+                    #('C', ['A', 'C'], 1)
+                    if child[0] not in visited:
+                        fringe.update(child, child[2] + problem.heuristic(child[0]))
+                    #print fringe.heap #DEBUG
+
+        #util.raiseNotDefined()
+
+def main():
+    """
+    Objetivo: Implementar todos los algoritmos de búsqueda vistos en clase.
+    Plantear adecuadamente el grafo de búsqueda anexado y encontrar la solución
+    de dicho grafo utilizando cada una de las funciones implementadas
+    (bfs, ucs y a*); DFS ha sido implementado por ti.
+
+    Además, presentar los recorridos realizados por sus algoritmos paso a paso
+    en hojas aparte para demostrar la correcta implementación de estos.
+
+    Para ejecutar un problema y probar su solución es necesario hacer lo siguiente:
+
+    # Crear una instancia de la clase 'Problem'. Verificar la definición
+    del método constructor para los parámetros que puede tomar.
+
+    # Definimos un problema con inicio en el nodo 'F' y meta en 'C' utilizando
+    # El espacio de estados G3 (O el grafo 3)
+
+    problem = Problem(goal='C', start = 'F', space = 'g3')
+
+    # Después, debes crear una instancia de la clase 'Solver'.
+
+    solver = Solver()
+
+    # Debes imprimir la salida de los métodos 'bfs', 'ucs' y 'astar'
+    # tal y como se muestra en las siguientes lineas
+
+    print("Solución: " + str(solver.dfs(problem)))
+    print("Nodos expandidos: " + str(problem.getNodesExpanded()))
+
+    # Para pasar un grafo declarado en cualquier otra parte, puedes hacer uso del
+    # parámetro 'use_graph' del método constructor de la clase problem,
+    # el cual es un diccionario como los demás grafos.
+
+    # ej.
+    problema = Problem(start = 'X', goal = 'Y', use_graph = mi_grafo)
+    solver = Solver()
+    solucion = solver.dfs(problema)
+    print "Resultado: %s"%(str(solucion))
+    print "Nodos expandidos: %i"%(problem.getNodesExpanded())
+    print "Costo: %i"%(problem.getSolutionCost(solucion))
+
+    #Para implementar A*, necesitas desarrollar una heuristica admisible
+    ( f(s) = g(s) + h(s) admisible <->  0 < h(s)<= g(s) )
+    # Una vez hayas escrito tu heuristica, definela en el método constructor
+    # del problema mediante el parámetro 'heur'
+    # ej.
+        def mi_heuristica(estado):
+            ...
+
+        problema = Problem(start = 'A', goal = 'F', space = 'g3', heur='mi_heuristica')
+        solucionador = Solver(problema)
+        solucionador.astar()
+            ...
+
+
+    Éxito y que te diviertas :)
+    """
+
+    """
+    Takes the total path cost and splits it in half
+    """
+    def dist_heur(node):
+        if isinstance(node, tuple()):
+            return problem.getSolutionCost(node[1])/2
+        else:
+            return 0
+
+
+    problem = Problem(start = 'A', goal='F', space='g3', heur=dist_heur)
+    solv = Solver()
+    print("--------------DFS----------------------")
+    sol = solv.dfs(problem)
+    print "Solution: %s" % (str(sol))
+    print "Nodes expanded: %s" % (str(problem.getNodesExpanded()))
+    print "Cost: %i" % (problem.getSolutionCost(sol))
+
+    print("--------------BFS----------------------")
+    sol = solv.bfs(problem)
+    print "Solution: %s" % (str(sol))
+    print "Nodes expanded: %s" % (str(problem.getNodesExpanded()))
+    print "Cost: %i" % (problem.getSolutionCost(sol))
+
+
+    print("--------------UCS----------------------")
+    sol = solv.ucs(problem)
+    print "Solution: %s" % (str(sol))
+    print "Nodes expanded: %s" % (str(problem.getNodesExpanded()))
+    print "Cost: %i" % (problem.getSolutionCost(sol))
+
+    print("--------------A*----------------------")
+    sol = solv.astar(problem)
+    print "Solution: %s" % (str(sol))
+    print "Nodes expanded: %s" % (str(problem.getNodesExpanded()))
+    print "Cost: %i" % (problem.getSolutionCost(sol))
+
+    print("--------------A* TSP----------------------")
+    problem_2 = solutions.SpaceSolution.problem
+    problem_2.setHeuristic(dist_heur)
+    sol = solv.astar(problem_2)
+    print "Solution: %s" % (str(sol))
+    print "Nodes expanded: %s" % (str(problem.getNodesExpanded()))
+    print "Cost: %i" % (problem.getSolutionCost(sol))
+
+
+
+if __name__ == '__main__':
+    main()
